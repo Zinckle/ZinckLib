c1d38db52429a5eea7a12e087028325d11e71759
Mitchell Zinck <58631706+Zinckle@users.noreply.github.com>
2023-01-21 00:35:48 -0500

    working on some tests for existing methods

--- tests/toNumber_tests.py
+++ tests/toNumber_tests.py
@@ -8,6 +8,22 @@ class ToNumbersTestCases(unittest.TestCase):
         result = toNumber("")
         self.assertEqual(result, [])
 
+    def test_space(self):
+        result = toNumber(" ")
+        self.assertEqual(result, [0])
+
+    def test_singleCharA(self):
+        result = toNumber("a")
+        self.assertEqual(result, [1])
+
+    def test_singleCharZ(self):
+        result = toNumber("z")
+        self.assertEqual(result, [26])
+
+    def test_MultipleCharABC(self):
+        result = toNumber("abc")
+        self.assertEqual(result, [1, 2, 3])
+
 
 if __name__ == '__main__':
     unittest.main()
