0dc4f48ecf2296a06b2a708977282440388b0fce
Mitchell Zinck <mzinck9@gmail.com>
2023-02-28 12:04:43 -0500

    still testing autodoc

--- src/ZinckLib/isAlphabetical.py
+++ src/ZinckLib/isAlphabetical.py
@@ -1,12 +1,6 @@
 def isAlphabetical(toCheck: str):
     """
-    Checks to see if a given string is in alphabetical order
-
-    :param toCheck: the string to be checked
-    :type toCheck: str
-
-    :return: true or false if it is alphabetical
-    :rtype: boolean
+   test
     """
     if type(toCheck) is not str:
         raise TypeError('Only strings are allowed')
