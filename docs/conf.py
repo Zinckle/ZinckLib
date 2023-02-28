b0847c1c84e512c0ce4fd8f884e174c043ac55b1
Mitchell Zinck <mzinck9@gmail.com>
2023-02-28 11:16:22 -0500

    testing autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -12,11 +12,8 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
+    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
 ]
 
 intersphinx_mapping = {
